674d8304888a2fad7f18e474cc4c083479ef298d
slopit <trevngor@gmail.com>
2025-10-09 13:04:06 -0400

    fixed automation
    asteroid data is not automated correctly and can be used for any asteroid in the horizon data base

--- get_horizons_rv.m
+++ get_horizons_rv.m
@@ -1,4 +1,4 @@
-function state_vec = get_horizons_rv(target_id, stop_date, step_size)
+function state_vec = get_horizons_rv(target_id, start_date, stop_date, step_size)
 % GET_HORIZONS_RV Queries JPL Horizons and returns the final [x y z vx vy vz] vector.
 %
 % Inputs:
@@ -10,7 +10,7 @@ function state_vec = get_horizons_rv(target_id, stop_date, step_size)
 % Output:
 %   state_vec  - 1x6 vector: [x y z vx vy vz] in [km, km/s]
 
-start_date = stop_date - days(1);
+% start_date = stop_date - days(1);
 stop_date = datestr(stop_date, 'yyyy-mm-ddTHH:MM:SS');
 start_date = datestr(start_date, 'yyyy-mm-ddTHH:MM:SS');
 
---
+++ horizonstests.m
@@ -0,0 +1,2 @@
+% Test File For comparing manual horizons data with web scraped 
+state_vec = get_horizons_rv('2024 PDC', '2032-Jul-07', '1 d')
